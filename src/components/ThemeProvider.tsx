8d455391f9bd6d89a1f11fc893f020f1cda8dc4e
Peter G <peterjonathang@gmail.com>
2025-03-18 22:04:37 -0700

    [css] add theme provider from emotion

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
+import { ReactNode } from "react";
+import {theme} from "@/styles/theme";
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+	return (
+		<EmotionThemeProvider theme={theme}>
+			{children}
+		</EmotionThemeProvider>
+	);
+}
