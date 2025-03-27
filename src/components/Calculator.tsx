e4f867a5a0ea95c1117038dd03e98c5662848fe7
Peter G <peterjonathang@gmail.com>
2025-03-26 21:06:26 -0700

    [chore] test context with calculator

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,2 +1,8 @@
-const Calculator = () => <div>Calculator Content</div>;
+import { useMeasurement } from "@/context/MeasurementContext";
+
+const Calculator = () => {
+	const { isMetric } = useMeasurement();
+	return <div>Using {isMetric ? "Metric" : "Imperial"} system</div>;
+};
+
 export default Calculator;
